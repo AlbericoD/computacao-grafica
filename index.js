4acc099a2e40a05f4efeb36cb564ab5be2977dcd
AlbericoD <albericodiasbarretofilho@gmail.com>
2020-10-09 14:39:44 -0300

    adicionado slider de fps.

--- index.js
+++ index.js
@@ -2,22 +2,36 @@ let quadrado = new Quadrado("#folha-de-desenho", 100);
 
 const botaoIniciar = document.querySelector(".btn-iniciar");
 const botaoResetar = document.querySelector(".btn-resetar");
+const fps = document.querySelector(".sliderFPS");
+const displayFPS = document.querySelector(".fps");
 
 function criarQuadrado() {
-  quadrado = new Quadrado("#folha-de-desenho", 100);
-  quadrado.desenha();
-  quadrado.cor("red");
+    quadrado = new Quadrado("#folha-de-desenho", 100);
+    quadrado.desenha();
+    quadrado.cor("red");
 }
 
 criarQuadrado();
 
+function iniciarAnimacao() {
+    quadrado.resetaPosicaoInicial();
+    criarQuadrado();
+    quadrado.movimentaParaDireita();
+}
+
+fps.oninput = function() {
+    displayFPS.textContent = fps.value
+    iniciarAnimacao()
+}
+
+
 botaoIniciar.addEventListener("click", () => {
-  quadrado.resetaPosicaoInicial();
-  criarQuadrado();
-  quadrado.movimentaParaDireita();
+    console.log('iniciando');
+    iniciarAnimacao();
 });
 
 botaoResetar.addEventListener("click", () => {
-  quadrado.resetaPosicaoInicial();
-  criarQuadrado();
+    console.log('resetando')
+    quadrado.resetaPosicaoInicial();
+    criarQuadrado();
 });
--- quadrado.js
+++ quadrado.js
@@ -1,94 +1,106 @@
 class Quadrado {
-  heightRatio = 1.5;
-  constructor(seletor, tamanho) {
-    this.canvas = document.querySelector(seletor);
-    this.informacaoEmX = document.querySelector(".posicaoX");
-    this.informacaoEmY = document.querySelector(".posicaoY");
-    this.informacaoTamanho = document.querySelector(".tamanho");
-    this.animacao = null;
-    if (this.canvas.getContext) this.contexto = this.canvas.getContext("2d");
+    heightRatio = 1.5;
+    constructor(seletor, tamanho) {
+        this.canvas = document.querySelector(seletor);
+        this.informacaoEmX = document.querySelector(".posicaoX");
+        this.informacaoEmY = document.querySelector(".posicaoY");
+        this.informacaoTamanho = document.querySelector(".tamanho");
+        this.animacao = null;
 
-    this.tamanho = tamanho;
+        if (this.canvas.getContext) this.contexto = this.canvas.getContext("2d");
 
-    const rect = this.canvas.parentNode.getBoundingClientRect();
-    this.canvas.width = rect.width;
-    this.canvas.height = rect.height;
+        this.tamanho = tamanho;
 
-    this.position = {
-      x: 0,
-      y: Math.ceil(this.canvas.height / 2),
-    };
-    this.informacaoTamanho.textContent = `${this.canvas.width}x${this.canvas.height}`;
-  }
-  limpaDesenho() {
-    const { x, y } = this.position;
-    this.contexto.clearRect(x, y, this.tamanho, this.tamanho);
-  }
-  desenha() {
-    const { x, y } = this.position;
-    this.informacaoEmX.textContent = this.canvas.getBoundingClientRect().x;
-    this.informacaoEmY.textContent = this.canvas.getBoundingClientRect().y;
-    this.contexto.fillRect(x, y, this.tamanho, this.tamanho);
-  }
-  cor(cor) {
-    this.limpaDesenho();
-    this.contexto.fillStyle = cor;
-    this.desenha();
-  }
-  movimentaParaDireita() {
-    clearInterval(this.animacao);
-    let counter = 0;
-    this.animacao = setInterval(() => {
-      if (counter <= this.canvas.getBoundingClientRect().x + 13) {
-        this.limpaDesenho();
-        this.position.x += 10;
-        this.desenha();
-        counter++;
-      } else {
-        clearInterval(this.animacao);
-        this.transformaEmEstrela();
-      }
-    }, 60);
-  }
+        const rect = this.canvas.parentNode.getBoundingClientRect();
+        this.canvas.width = rect.width;
+        this.canvas.height = rect.height;
 
-  resetaPosicaoInicial() {
-    clearInterval(this.animacao);
-    this.limpaDesenho();
-  }
-
-  criarEstrela(cx, cy, spikes, outerRadius, innerRadius) {
-    var rot = (Math.PI / 2) * 3;
-    var x = cx;
-    var y = cy;
-    var step = Math.PI / spikes;
-
-    this.contexto.strokeSyle = "#000";
-    this.contexto.beginPath();
-    this.contexto.moveTo(cx, cy - outerRadius);
-
-    for (let i = 0; i < spikes; i++) {
-      x = cx + Math.cos(rot) * outerRadius;
-      y = cy + Math.sin(rot) * outerRadius;
-      this.contexto.lineTo(x, y);
-      rot += step;
-
-      x = cx + Math.cos(rot) * innerRadius;
-      y = cy + Math.sin(rot) * innerRadius;
-      this.contexto.lineTo(x, y);
-      rot += step;
+        this.position = {
+            x: 0,
+            y: Math.ceil(this.canvas.height / 2),
+        };
+        this.informacaoTamanho.textContent = `${this.canvas.width}x${this.canvas.height}`;
     }
-    this.contexto.lineTo(cx, cy - outerRadius);
-    this.contexto.closePath();
-    this.limpaDesenho();
-    this.contexto.lineWidth = 5;
-    this.contexto.strokeStyle = "blue";
-    this.contexto.stroke();
-    this.contexto.fillStyle = "skyblue";
-    this.contexto.fill();
-  }
-  transformaEmEstrela() {
-    const { x, y } = this.position;
+    limpaDesenho() {
+        const { x, y } = this.position;
+        this.contexto.clearRect(x, y, this.tamanho, this.tamanho);
+    }
+    desenha() {
+        const { x, y } = this.position;
 
-    this.criarEstrela(x, y, 5, 30, this.tamanho);
-  }
+        this.contexto.fillRect(x, y, this.tamanho, this.tamanho);
+    }
+    cor(cor) {
+        this.limpaDesenho();
+        this.contexto.fillStyle = cor;
+        this.desenha();
+    }
+
+    atualizaInformacoes({ x, y }) {
+        this.informacaoEmX.textContent = x;
+        this.informacaoEmY.textContent = y;
+    }
+
+    mudaFPS(fps) {
+        this.informacaoFPS += fps;
+    }
+
+    movimentaParaDireita() {
+        clearInterval(this.animacao);
+        const fps = parseInt(document.querySelector(".sliderFPS").value)
+        let counter = 0;
+        this.animacao = setInterval(() => {
+            const currentInfo = this.canvas.getBoundingClientRect();
+            if (counter <= Math.round(currentInfo.width / 2)) {
+                this.limpaDesenho();
+                this.position.x += 1;
+                this.atualizaInformacoes(this.position)
+                this.desenha();
+                counter++;
+            } else {
+                clearInterval(this.animacao);
+                this.transformaEmEstrela();
+            }
+        }, Math.round(1000 / fps));
+    }
+
+    resetaPosicaoInicial() {
+        clearInterval(this.animacao);
+        this.limpaDesenho();
+    }
+
+    criarEstrela(cx, cy, spikes, outerRadius, innerRadius) {
+        let rot = (Math.PI / 2) * 3;
+        let x = cx;
+        let y = cy;
+        let step = Math.PI / spikes;
+
+        this.contexto.strokeSyle = "#000";
+        this.contexto.beginPath();
+        this.contexto.moveTo(cx, cy - outerRadius);
+
+        for (let i = 0; i < spikes; i++) {
+            x = cx + Math.cos(rot) * outerRadius;
+            y = cy + Math.sin(rot) * outerRadius;
+            this.contexto.lineTo(x, y);
+            rot += step;
+
+            x = cx + Math.cos(rot) * innerRadius;
+            y = cy + Math.sin(rot) * innerRadius;
+            this.contexto.lineTo(x, y);
+            rot += step;
+        }
+        this.contexto.lineTo(cx, cy - outerRadius);
+        this.contexto.closePath();
+        this.limpaDesenho();
+        this.contexto.lineWidth = 5;
+        this.contexto.strokeStyle = "blue";
+        this.contexto.stroke();
+        this.contexto.fillStyle = "skyblue";
+        this.contexto.fill();
+    }
+    transformaEmEstrela() {
+        const { x, y } = this.position;
+        this.criarEstrela(x, y, 5, 30, this.tamanho);
+    }
 }
